21f993c5048a81144d7646a77c7152edf7b756b5
Surender Jangir <Sam1989@users.noreply.github.com>
2026-09-14 12:32:32 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.kt
@@ -0,0 +1,28 @@
+class MinStack() {
+
+    var stack = Stack<Int>()
+    var minStack = Stack<Int>()
+
+    fun push(value: Int) {
+        stack.push(value)
+
+        if(minStack.isEmpty() || value <= minStack.peek()){
+            minStack.push(value)
+        }
+    }
+
+    fun pop() {
+        val popped = stack.pop()
+        if(popped == minStack.peek()) {
+            minStack.pop()
+        }
+    }
+
+    fun top(): Int {
+        return stack.peek()
+    }
+
+    fun getMin(): Int {
+        return minStack.peek()
+    }
+}
