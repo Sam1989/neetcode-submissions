2fad1f034d601918a2ed691304f28a310e41328a
Surender Jangir <Sam1989@users.noreply.github.com>
2026-09-18 11:26:48 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun search(nums: IntArray, target: Int): Int {
+        var low = 0
+        var high = nums.size - 1
+
+        while(low <= high){
+            var mid  = (low + high) / 2
+            when {
+                nums[mid] == target -> return mid
+                nums[mid] < target -> low = mid + 1
+                else ->  high = mid - 1
+            }     
+        }
+        return -1
+    }
+}
